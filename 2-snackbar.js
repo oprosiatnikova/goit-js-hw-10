34f63052441326f4c49cb307579eefbd16f46377
oprosiatnikova <61869228+oprosiatnikova@users.noreply.github.com>
2025-02-16 18:46:39 +0000

    Deploying to gh-pages from @ oprosiatnikova/goit-js-hw-10@b4e995587b96d8cc8d99d98ea1a426bf6890f268 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as s}from"./assets/vendor-BbbuE1sJ.js";document.querySelector(".form").addEventListener("submit",function(i){i.preventDefault();const t=parseInt(this.elements.delay.value),o=this.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(t):r(t)},t)}).then(e=>{s.success({title:"Успіх",message:`✅ Проміс виконано через ${e} мс`})}).catch(e=>{s.error({title:"Помилка",message:`❌ Проміс відхилено через ${e} мс`})})});
+//# sourceMappingURL=2-snackbar.js.map
